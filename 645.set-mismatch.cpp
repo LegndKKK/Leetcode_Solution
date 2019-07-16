634c6aa4b7be0f584355b53d8f560b286c560091
Legend_Kai <zjk1415926@gmail.com>
2019-07-17 00:24:38 +0800

    Array

--- 645.set-mismatch.cpp
+++ 645.set-mismatch.cpp
@@ -8,12 +8,17 @@ class Solution
 public:
     vector<int> findErrorNums(vector<int> &nums)
     {
+        //finding duplicate
+        //multiplying the number in index i by -1
+        //finding a number < 0 means already met before
         int dup = -1, missing = -1;
         for (int i : nums)
             if (nums[abs(i) - 1] < 0)
                 dup = abs(i);
             else
                 nums[abs(i) - 1] *= -1;
+        //finding missing
+        //checking number in array, finding a number > 0 means missing
         for (int i = 0; i < nums.size(); i++)
             if (nums[i] > 0)
             {
