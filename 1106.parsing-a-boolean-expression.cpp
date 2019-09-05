2508c3f87f604be33bbdc59f78db324e88166d79
Legend_Kai <zjk1415926@gmail.com>
2019-09-05 01:41:15 -0400

    String, Recursion

--- 1106.parsing-a-boolean-expression.cpp
+++ 1106.parsing-a-boolean-expression.cpp
@@ -8,9 +8,41 @@ class Solution
 public:
     bool parseBoolExpr(string expression)
     {
-        int ans = 0;
-        
-        return ans;
+        if (expression == "t")
+            return true;
+        if (expression == "f")
+            return false;
+        if (expression[0] == '!')
+            return !parseBoolExpr(expression.substr(2, expression.size() - 3));
+        bool ans = true;
+        int count = 0, last = 2;
+        vector<bool> sub;
+        for (int i = 2; i < expression.size() - 1; i++)
+        {
+            if (expression[i] == '(')
+                count++;
+            else if (expression[i] == ')')
+                count--;
+            else if (count == 0 && expression[i] == ',')
+            {
+                sub.push_back(parseBoolExpr(expression.substr(last, i - last)));
+                last = i + 1;
+            }
+        }
+        sub.push_back(parseBoolExpr(expression.substr(last, expression.size() - 1 - last)));
+        if (expression[0] == '&')
+        {
+            ans = true;
+            for (auto i : sub)
+                ans &= i;
+        }
+        else
+        {
+            ans = false;
+            for (auto i : sub)
+                ans |= i;
+        }
 
+        return ans;
     }
 };
