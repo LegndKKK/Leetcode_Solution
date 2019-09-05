c708ec29d41d546187ad44d41727985d49570ead
Legend_Kai <zjk1415926@gmail.com>
2019-09-05 01:41:43 -0400

    stack

--- 1111.maximum-nesting-depth-of-two-valid-parentheses-strings.cpp
+++ 1111.maximum-nesting-depth-of-two-valid-parentheses-strings.cpp
@@ -3,10 +3,49 @@
  *
  * [1111] Maximum Nesting Depth of Two Valid Parentheses Strings
  */
-class Solution {
+class Solution
+{
 public:
-    vector<int> maxDepthAfterSplit(string seq) {
-        
+    vector<int> maxDepthAfterSplit(string seq)
+    {
+        vector<int> ans;
+        stack<int> s;
+        //int maxn = 0, cur = 0;
+        // for (char c : seq)
+        //     if (c == '(')
+        //     {
+        //         s.push(cur);
+        //         cur++;
+        //         maxn = max(maxn, cur);
+        //     }
+        //     else
+        //     {
+        //         s.pop();
+        //         cur--;
+        //     }
+        // //cout << maxn << endl;
+        // maxn = (maxn + 1) / 2;
+        int cur = 0;
+        for (char c : seq)
+            if (c == '(')
+            {
+                s.push(cur);
+                if (cur % 2 == 0)
+                    ans.push_back(0);
+                else
+                    ans.push_back(1);
+                cur++;
+            }
+            else
+            {
+                if (s.top() % 2 == 0)
+                    ans.push_back(0);
+                else
+                    ans.push_back(1);
+                s.pop();
+                cur--;
+            }
+        return ans;
     }
 };
-
+//"(()())((()))"
