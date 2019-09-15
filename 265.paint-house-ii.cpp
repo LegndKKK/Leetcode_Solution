0fc8a3c1ca306fe530f3d4ff7d1a45b605e0c1c4
Legend_Kai <zjk1415926@gmail.com>
2019-09-15 01:59:58 -0400

    DP

--- 265.paint-house-ii.cpp
+++ 265.paint-house-ii.cpp
@@ -8,55 +8,34 @@ class Solution
 public:
     int minCostII(vector<vector<int>> &costs)
     {
-        // int min1 = INT_MAX, min2 = INT_MAX, ans = INT_MAX;
-        // int row1 = 0, row2 = 0;
-        int ans = INT_MAX;
-        vector<int> pre, cur;
+        int premin = 0, premin2 = 0, preidx = -1;
         int n = costs.size(), k = n > 0 ? costs[0].size() : 0;
         if (!n || !k)
             return 0;
 
-        function<vector<int>(vector<int>)> searchmin = [](vector<int> arr) {
-            vector<int> ret(4, INT_MAX);
-            for (int i = 0; i < arr.size(); i++)
-                if (arr[i] < ret[0])
-                {
-                    ret[2] = ret[0];
-                    ret[3] = ret[1];
-                    ret[0] = arr[i];
-                    ret[1] = i;
-                }
-                else if (arr[i] < ret[2])
-                {
-                    ret[2] = arr[i];
-                    ret[3] = i;
-                }
-            return ret;
-        };
-
-        pre = searchmin(costs[0]);
-
-        for (int num = 1; num < n; num++)
+        for (int i = 0; i < n; i++)
         {
-            cur = searchmin(costs[num]);
-            //cout << pre[0] << cur[0] << endl;
-            vector<pair<int, int>> temp;
-            if (cur[1] != pre[1])
-                temp.push_back({cur[0] + pre[0], cur[1]});
-            if (cur[3] != pre[1])
-                temp.push_back({cur[2] + pre[0], cur[3]});
-            if (cur[1] != pre[3])
-                temp.push_back({cur[0] + pre[2], cur[1]});
-            if (cur[3] != pre[3])
-                temp.push_back({cur[2] + pre[2], cur[1]});
-            //for (auto i : temp)
-            //    cout << i.first << " " << i.second << endl;
-            sort(temp.begin(), temp.end(), [](pair<int, int> a, pair<int, int> b) { return a.first < b.first; });
-            pre[0] = temp[0].first;
-            pre[1] = temp[0].second;
-            pre[2] = temp[1].first;
-            pre[3] = temp[1].second;
+            int min = INT_MAX, min2 = INT_MAX, idx = 0;
+            for (int j = 0; j < k; j++)
+            {
+                int temp = costs[i][j];
+                temp += j == preidx ? premin2 : premin;
+                if (temp <= min)
+                {
+                    min2 = min;
+                    min = temp;
+                    idx = j;
+                }
+                else if (temp < min2)
+                {
+                    min2 = temp;
+                }
+            }
+            premin = min;
+            premin2 = min2;
+            preidx = idx;
         }
-        return pre[0];
+
+        return premin;
     }
 };
