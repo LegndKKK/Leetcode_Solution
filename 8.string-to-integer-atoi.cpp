98aaf120086d703234823fe7ce8b04aebccc3bb7
Legend_Kai <zjk1415926@gmail.com>
2019-10-02 15:17:52 -0400

    String

--- 8.string-to-integer-atoi.cpp
+++ 8.string-to-integer-atoi.cpp
@@ -3,10 +3,36 @@
  *
  * [8] String to Integer (atoi)
  */
-class Solution {
+class Solution
+{
 public:
-    int myAtoi(string str) {
-        
+    int myAtoi(string str)
+    {
+        int n = str.size(), i = 0, minus = 0;
+        long long ans = 0;
+        while (i < n && str[i] == ' ')
+            i++;
+        str = str.substr(i, n - i);
+        n -= i;
+        if (n > 0 && (str[0] == '-' || str[0] == '+'))
+        {
+            minus = str[0] == '-' ? 1 : 0;
+            str = str.substr(1, n - 1);
+            n--;
+        }
+        if (n < 1 || !(str[0] >= '0' && str[0] <= '9'))
+            return 0;
+        i = 0;
+        while (i < n && str[i] >= '0' && str[i] <= '9')
+        {
+            ans *= 10;
+            ans += (str[i] - '0');
+            i++;
+            if (minus && ans > INT_MAX)
+                return INT_MIN;
+            else if (!minus && ans >= INT_MAX)
+                return INT_MAX;
+        }
+        return minus ? -ans : ans;
     }
 };
-
