fa103f898c905c992c46b830e395674c3b3e980a
Legend_Kai <zjk1415926@gmail.com>
2019-10-07 02:02:46 -0400

    Stack

--- 1190.reverse-substrings-between-each-pair-of-parentheses.cpp
+++ 1190.reverse-substrings-between-each-pair-of-parentheses.cpp
@@ -5,11 +5,50 @@
  */
 
 // @lc code=start
-class Solution {
+class Solution
+{
 public:
-    string reverseParentheses(string s) {
-        
+    string reverseParentheses(string s)
+    {
+        stack<string> ss;
+        if (s.size() < 2)
+            return s;
+        for (char c : s)
+        {
+            if (c == '(')
+                ss.push(string(1, c));
+            else if (c == ')')
+            {
+                if (!ss.empty() && ss.top() == "(")
+                {
+                    ss.pop();
+                    continue;
+                }
+                string temp = ss.top();
+                reverse(temp.begin(), temp.end());
+                ss.pop();
+                ss.pop();
+                if (!ss.empty())
+                {
+                    temp = ss.top() + temp;
+                    ss.pop();
+                }
+                ss.push(temp);
+            }
+            else
+            {
+                if (ss.empty() || (!s.empty() && ss.top() == "("))
+                    ss.push(string(1, c));
+                else
+                {
+                    string temp = ss.top();
+                    ss.pop();
+                    temp += c;
+                    ss.push(temp);
+                }
+            }
+        }
+        return ss.top();
     }
 };
 // @lc code=end
-
