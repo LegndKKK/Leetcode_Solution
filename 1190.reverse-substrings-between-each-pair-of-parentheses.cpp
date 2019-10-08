cfb30c1014f5f03ea41341846e24cf025412cc3f
Legend_Kai <zjk1415926@gmail.com>
2019-10-07 20:16:28 -0400

    Stack

--- 1190.reverse-substrings-between-each-pair-of-parentheses.cpp
+++ 1190.reverse-substrings-between-each-pair-of-parentheses.cpp
@@ -15,32 +15,40 @@ public:
             return s;
         for (char c : s)
         {
+            //""+c is not converting a char to string!!!!
             if (c == '(')
                 ss.push(string(1, c));
             else if (c == ')')
             {
+                //check if top = "(", just pop
                 if (!ss.empty() && ss.top() == "(")
                 {
                     ss.pop();
                     continue;
                 }
+                //top is string, reverse
                 string temp = ss.top();
                 reverse(temp.begin(), temp.end());
+                //pop string and (
                 ss.pop();
                 ss.pop();
-                if (!ss.empty())
+                //if top is string, combine two strings
+                if (!ss.empty() && ss.top() != "(")
                 {
                     temp = ss.top() + temp;
                     ss.pop();
                 }
+                //push new string into stack
                 ss.push(temp);
             }
             else
             {
+                //if empty or first character
                 if (ss.empty() || (!s.empty() && ss.top() == "("))
                     ss.push(string(1, c));
                 else
                 {
+                    //if top is stirng, add char to the end of string
                     string temp = ss.top();
                     ss.pop();
                     temp += c;
