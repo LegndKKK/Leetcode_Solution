c104beaf041a8312f14df8fb3ae772c851af8862
Legend_Kai <zjk1415926@gmail.com>
2019-10-19 01:34:43 -0400

    Topo sort

--- 269.alien-dictionary.cpp
+++ 269.alien-dictionary.cpp
@@ -5,11 +5,60 @@
  */
 
 // @lc code=start
-class Solution {
+class Solution
+{
 public:
-    string alienOrder(vector<string>& words) {
-        
+    string alienOrder(vector<string> &words)
+    {
+        vector<set<int>> in(26), out(26);
+        int visit[26] = {0}, left = 0;
+        string ans = "";
+        for (string s : words)
+            for (char c : s)
+                visit[c - 'a'] = 1;
+        for (int i = 0; i < words.size() - 1; i++)
+        {
+            string str1 = words[i];
+            string str2 = words[i + 1];
+            for (int j = 0; j < min(str1.size(), str2.size()); j++)
+            {
+                if (str1[j] != str2[j])
+                {
+                    in[str2[j] - 'a'].insert(str1[j] - 'a');
+                    out[str1[j] - 'a'].insert(str2[j] - 'a');
+                    break;
+                }
+            }
+        }
+        for (int i : visit)
+            if (i)
+                left++;
+        while (true)
+        {
+            bool flag = false;
+            for (int i = 0; i < 26; i++)
+            {
+                if (visit[i] && in[i].size() == 0)
+                {
+                    ans += (char)(i + 'a');
+                    auto it = out[i].begin();
+                    while (it != out[i].end())
+                    {
+                        in[(*it)].erase(i);
+                        it++;
+                    }
+                    visit[i] = 0;
+                    flag = true;
+                    left--;
+                    if (left == 0)
+                        return ans;
+                }
+            }
+            if (!flag)
+                return "";
+        }
+
+        return ans;
     }
 };
 // @lc code=end
-
